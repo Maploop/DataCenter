1dc60037c42cd4465e7a0af9a878d4e391b3df16
Maploop <sweatypvpmc@gmail.com>
2024-01-11 12:49:15 +0330

    Intelligence Center

---
+++ script/main_page.js
@@ -0,0 +1,140 @@
+var _allText = '';
+
+window.addEventListener('load', (e) => {
+    fetch('https://raw.githubusercontent.com/Maploop/DataCenter/main/very_important.csv').then(res => res.text()).then((text) => {
+      load_page(text);
+    });
+});
+
+window.addEventListener('keypress', (e) => {
+    if (e.key == 'Enter') {
+        let query = document.getElementById("search-query").value;
+        if (!query) {
+            document.querySelector('#no-valid-search').style.display = 'block';
+            return;
+        }
+        commence_search(query);
+    }
+})
+
+window.addEventListener('submit', (e) => {
+    let query = document.getElementById("search-query").value;
+    if (!query) {
+        document.querySelector('#no-valid-search').style.display = 'block';
+        return;
+    }
+    commence_search(query);
+})
+
+document.getElementById('search-submit').onclick = (e) => {
+    let query = document.getElementById("search-query").value;
+    if (!query) {
+        document.querySelector('#no-valid-search').style.display = 'block';
+        return;
+    }
+    commence_search(query);
+}
+
+/**
+ * 
+ * @param {string} keyword 
+ */
+function commence_search(keyword) {
+    document.querySelector("#box").innerHTML = `<h3 style="color: lightblue">Loading results...</h3>`;
+    let lines = _allText.split('\n');
+
+    var html = ``;
+    let resCount = 0;
+    for (let index in lines) {
+        if (index == 0) continue;
+        let line = lines[index];
+
+        let splitted = line.split(',');
+        let name = splitted[0];
+        if (!name.toLowerCase().includes(keyword.toLowerCase())) continue;
+        let phone = splitted[1];
+        let instagram = splitted[2];
+        let telegram = splitted[3];
+        let mom = splitted[4];
+        let dad = splitted[5];
+        let address = splitted[6];
+        let relatives = splitted[7];
+        let additional = splitted[8];
+
+        const our = `
+        <div class="item">
+                <h4 class="item-title">${name}</h4>
+                <p class="item-phone">Phone Number: >${phone}</p>
+                <p class="item-instagram">Instagram: >${instagram}</p>
+                <p class="item-telegram">Telegram: >${telegram}</p>
+                <p class="item-mom">Mom: >${mom}</p>
+                <p class="item-dad">Dad:>${dad}</p>
+                <p class="item-address">Address: >${address}</p>
+                <p class="item-other_relatives">Other Relatives: >${relatives}</p>
+                <p class="item-additional_information">Additional INFO: >${additional}</p>
+            </div>`
+
+       html += our;
+
+        console.log(name);
+        resCount++;
+    }
+
+    if (resCount <= 0) {
+        html = `<h3 style="color: red">Whoops! Looks like there are no results!</h3>
+        <h3 style="color: red">Error: CSV_FILE_NULLIFIED</h3>`;
+    }
+    document.querySelector("#case-count").innerHTML = resCount;
+    document.querySelector("#box").innerHTML = html;
+}
+
+/**
+ * 
+ * @param {string} text 
+ */
+function load_page(text) {
+    let lines = text.split('\n');
+
+    var html = `<h3 style="color: red">Whoops! Looks like there are no results!</h3>
+    <h3 style="color: red">Error: CSV_FILE_NULLIFIED</h3>`;
+    let resCount = lines.length - 1;
+    if (resCount > 0) {
+        html = '';
+    }
+    document.querySelector("#case-count").innerHTML = resCount;
+    for (let index in lines) {
+        if (index == 0) continue;
+        let line = lines[index];
+
+        let splitted = line.split(',');
+        let name = splitted[0];
+        let phone = splitted[1];
+        let instagram = splitted[2];
+        let telegram = splitted[3];
+        let mom = splitted[4];
+        let dad = splitted[5];
+        let address = splitted[6];
+        let relatives = splitted[7];
+        let additional = splitted[8];
+
+        const our = `
+        <div class="item">
+                <h4 class="item-title">${name}</h4>
+                <p class="item-phone">Phone Number: >${phone}</p>
+                <p class="item-instagram">Instagram: >${instagram}</p>
+                <p class="item-telegram">Telegram: >${telegram}</p>
+                <p class="item-mom">Mom: >${mom}</p>
+                <p class="item-dad">Dad:>${dad}</p>
+                <p class="item-address">Address: >${address}</p>
+                <p class="item-other_relatives">Other Relatives: >${relatives}</p>
+                <p class="item-additional_information">Additional INFO: >${additional}</p>
+            </div>`
+
+       html += our;
+
+        console.log(name);
+    }
+
+    document.querySelector("#box").innerHTML = html;
+    _allText = text;
+}
