961802dadc56a398bb7eeef5583c1949ccee0a11
Maploop <sweatypvpmc@gmail.com>
2024-11-29 18:11:43 +0330

    cookies fix

--- ARCHIVE/script/Main.js
+++ ARCHIVE/script/Main.js
@@ -9,62 +9,89 @@ const _INT_CONFIG_CardLinkColor = "yellow";
 const _INT_CONFIG_ThumbnailIcon = "./ARCHIVE/icon/tn2.png";
 
 // LOCAL UTILITY FUNCTIONS
-function BAD_Notif(what) {}
-function LocalFunc_Show(what) { document.querySelector('#' + what).style.display = 'block'; }
-function LocalFunc_GetValue(what) { return document.getElementById(what).value; }
+function BAD_Notif(what) { }
+function LocalFunc_Show(what) {
+    document.querySelector("#" + what).style.display = "block";
+}
+function LocalFunc_GetValue(what) {
+    return document.getElementById(what).value;
+}
 
-window.addEventListener('load', (e) => {
-    fetch('./important.json').then(res => res.json()).then((text) => {
-        console.log(text);
-      load_page(text);
-    });
-    fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        discord_message("https://discord.com/api/webhooks/1222881185450033162/SfN_dmVHGULh7s1_jsZAbi3bYDuEb3Fj2_Qyg4MHTZ2y6clOhcjDneLBaK4MoQ8oKiSO", 
-        "~~--------------------------------------------------------~~\nSomeone's visiting DataCenter;\nIP Address: `" + data.ip + "`\n" + 
-        "Check the address INFO: <https://ip-api.com/#" + data.ip + ">\n~~--------------------------------------------------------~~");
-    })
-    .catch(error => {
-        console.log('Error:', error);
-        discord_message("https://discord.com/api/webhooks/1222881185450033162/SfN_dmVHGULh7s1_jsZAbi3bYDuEb3Fj2_Qyg4MHTZ2y6clOhcjDneLBaK4MoQ8oKiSO", 
-        "``` Someone's visiting DataCenter; (Unknown Origin)``` " + error);
-    });
+window.addEventListener("load", (e) => {
+    fetch("./important.json")
+        .then((res) => res.json())
+        .then((text) => {
+            console.log(text);
+            load_page(text);
+        })
+        .catch((err) => {
+            document.querySelector("#no-valid-search").style.display = "block";
+            console.log(err);
+        });
+    fetch("https://api.ipify.org?format=json")
+        .then((response) => response.json())
+        .then((data) => {
+            const visitedTimeLast30Days = getCookie("visited");
+            discord_message(
+                "https://discord.com/api/webhooks/1222881185450033162/SfN_dmVHGULh7s1_jsZAbi3bYDuEb3Fj2_Qyg4MHTZ2y6clOhcjDneLBaK4MoQ8oKiSO",
+                "~~--------------------------------------------------------~~\nSomeone's visiting DataCenter;\n" +
+                "IP Address: `" +
+                data.ip +
+                "`\n" +
+                "Last Visited (BD): " +
+                (visitedTimeLast30Days
+                    ? `<t:${visitedTimeLast30Days}:F>`
+                    : "**Newcomer!**") +
+                "\n" +
+                "Check the address INFO: <https://ip-api.com/#" +
+                data.ip +
+                ">\n~~--------------------------------------------------------~~",
+            );
+            setCookie("visited", Date.now(), 9999);
+        })
+        .catch((error) => {
+            console.log("Error:", error);
+            discord_message(
+                "https://discord.com/api/webhooks/1222881185450033162/SfN_dmVHGULh7s1_jsZAbi3bYDuEb3Fj2_Qyg4MHTZ2y6clOhcjDneLBaK4MoQ8oKiSO",
+                "``` Someone's visiting DataCenter; (Unknown Origin)``` " + error,
+            );
+        });
 });
 
-window.addEventListener('keypress', (e) => {
-    if (e.key == 'Enter') {
+window.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
         let query = LocalFunc_GetValue("search-query");
         if (!query) {
             return LocalFunc_Show("no-valid-search");
         }
         commence_search(query);
     }
-})
+});
 
-window.addEventListener('submit', (e) => {
+window.addEventListener("submit", (e) => {
     let query = LocalFunc_GetValue("search-query");
     if (!query) {
         return LocalFunc_Show("no-valid-search");
     }
     commence_search(query);
-})
+});
 
-document.getElementById('search-submit').onclick = (e) => {
+document.getElementById("search-submit").onclick = (e) => {
     let query = LocalFunc_GetValue("search-query");
     if (!query) {
         return LocalFunc_Show("no-valid-search");
     }
     commence_search(query);
-}
+};
 
 /**
- * 
- * @param {string} keyword 
+ *
+ * @param {string} keyword
  */
 function commence_search(keyword) {
     console.log("Keyword is " + keyword);
-    document.querySelector("#box").innerHTML = `<span class="cyber-att fg-cyan">ATTENTION: Loading Results..</span>`;
+    document.querySelector("#box").innerHTML =
+        `<span class="cyber-att fg-cyan">ATTENTION: Loading Results..</span>`;
 
     var html = ``;
     let resCount = 0;
@@ -73,28 +100,27 @@ function commence_search(keyword) {
     var _first = true;
     for (let i = 0; i < j_object.length; i++) {
         let ts = j_object[i];
-        
-        let label = ts['label'];
+
+        let label = ts["label"];
         if (!label.toLowerCase().includes(keyword.toLowerCase())) continue;
-        let icon = ts['image_icon'];
+        let icon = ts["image_icon"];
         if (icon == "NOIMAGE") {
             icon = _INT_CONFIG_ThumbnailIcon;
         }
-        
+
         var data = "";
-        Object.keys(ts['data']).forEach((key) => {
-            if (ts['data'][key].startsWith("link$")) {
+        Object.keys(ts["data"]).forEach((key) => {
+            if (ts["data"][key].startsWith("link$")) {
                 var keyNorm = make_string_normal(key);
-                var stripped = strip_this(ts['data'][key]).replace("link$", "");
+                var stripped = strip_this(ts["data"][key]).replace("link$", "");
                 data += `<label>${keyNorm}: <a class="fg-${_INT_CONFIG_CardLinkColor}" href="${stripped}">${stripped}</a></label>`;
-            }
-            else if (ts['data'][key].startsWith("attachment$")) {
+            } else if (ts["data"][key].startsWith("attachment$")) {
                 var keyNorm = make_string_normal(key);
-                var stripped = strip_this(ts['data'][key]);
+                var stripped = strip_this(ts["data"][key]);
                 data += `<label>${keyNorm}: <a class="fg-${_INT_CONFIG_CardLinkColor}" href="${stripped}">Attachment</a></label>`;
             } else {
                 var keyNorm = make_string_normal(key);
-                var stripped = ts['data'][key];
+                var stripped = ts["data"][key];
                 data += `<label>${key}: ${stripped}</label>`;
             }
         });
@@ -131,7 +157,7 @@ function commence_search(keyword) {
         html += ourNewData;
         _col_count++;
         resCount++;
-        console.log(_col_count)
+        console.log(_col_count);
     }
 
     if (resCount <= 0) {
@@ -142,8 +168,8 @@ function commence_search(keyword) {
 }
 
 /**
- * 
- * @param {JSONObject} text 
+ *
+ * @param {JSONObject} text
  */
 function load_page(json) {
     j_object = json;
@@ -153,7 +179,7 @@ function load_page(json) {
     <h3 style="color: red">Error: DATA_NULLIFIED</h3>`;
     let resCount = lines;
     if (resCount > 0) {
-        html = '';
+        html = "";
     }
     document.querySelector("#case-count").innerHTML = resCount;
 
@@ -161,27 +187,26 @@ function load_page(json) {
     var _first = true;
     for (let i = 0; i < json.length; i++) {
         let ts = j_object[i];
-        
-        let label = ts['label'];
-        let icon = ts['image_icon'];
+
+        let label = ts["label"];
+        let icon = ts["image_icon"];
         if (icon == "NOIMAGE") {
             icon = _INT_CONFIG_ThumbnailIcon;
         }
-        
+
         var data = "";
-        Object.keys(ts['data']).forEach((key) => {
-            if (ts['data'][key].startsWith("link$")) {
+        Object.keys(ts["data"]).forEach((key) => {
+            if (ts["data"][key].startsWith("link$")) {
                 var keyNorm = make_string_normal(key);
-                var stripped = strip_this(ts['data'][key]).replace("link$", "");
+                var stripped = strip_this(ts["data"][key]).replace("link$", "");
                 data += `<label>${keyNorm}: <a class="fg-${_INT_CONFIG_CardLinkColor}" href="${stripped}">${stripped}</a></label>`;
-            }
-            else if (ts['data'][key].startsWith("attachment$")) {
+            } else if (ts["data"][key].startsWith("attachment$")) {
                 var keyNorm = make_string_normal(key);
-                var stripped = strip_this(ts['data'][key]);
+                var stripped = strip_this(ts["data"][key]);
                 data += `<label>${keyNorm}: <a class="fg-${_INT_CONFIG_CardLinkColor}" href="${stripped}">Attachment</a></label>`;
             } else {
                 var keyNorm = make_string_normal(key);
-                var stripped = ts['data'][key];
+                var stripped = ts["data"][key];
                 data += `<label>${key}: ${stripped}</label>`;
             }
         });
@@ -217,7 +242,7 @@ function load_page(json) {
 
         html += ourNewData;
         _col_count++;
-        console.log(_col_count)
+        console.log(_col_count);
     }
 
     document.querySelector("#box").innerHTML = html;
@@ -225,8 +250,8 @@ function load_page(json) {
 }
 
 /**
- * 
- * @param {string} str 
+ *
+ * @param {string} str
  * @returns string
  */
 function make_string_normal(str) {
@@ -240,9 +265,34 @@ function strip_this(str) {
 function discord_message(webHookURL, message) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", webHookURL, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        'content': message,
-        'username':'DataCenter Visiting History',
-    }));
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(
+        JSON.stringify({
+            content: message,
+            username: "DataCenter Visiting History",
+        }),
+    );
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
--- script/main_page.js
+++ script/main_page.js
@@ -1,83 +1,105 @@
 var j_object;
-window.addEventListener('load', (e) => {
-    fetch('./important.json').then(res => res.json()).then((text) => {
-        console.log(text);
-      load_page(text);
-    });
-    fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        discord_message("https://discord.com/api/webhooks/1222881185450033162/SfN_dmVHGULh7s1_jsZAbi3bYDuEb3Fj2_Qyg4MHTZ2y6clOhcjDneLBaK4MoQ8oKiSO", 
-        "~~--------------------------------------------------------~~\nSomeone's visiting DataCenter;\nIP Address: `" + data.ip + "`\n" + 
-        "Check the address INFO: <https://ip-api.com/#" + data.ip + ">\n~~--------------------------------------------------------~~");
-    })
-    .catch(error => {
-        console.log('Error:', error);
-        discord_message("https://discord.com/api/webhooks/1222881185450033162/SfN_dmVHGULh7s1_jsZAbi3bYDuEb3Fj2_Qyg4MHTZ2y6clOhcjDneLBaK4MoQ8oKiSO", 
-        "``` Someone's visiting DataCenter; (Unknown Origin)``` " + error);
-    });
+window.addEventListener("load", (e) => {
+    fetch("./important.json", { mode: "no-cors" })
+        .then((res) => res.json())
+        .then((text) => {
+            console.log(text);
+            load_page(text);
+        })
+        .catch((err) => {
+            document.querySelector("#no-valid-search").style.display = "block";
+            console.log(err);
+        });
+    fetch("https://api.ipify.org?format=json")
+        .then((response) => response.json())
+        .then((data) => {
+            const visitedTimeLast30Days = getCookie("visited");
+            discord_message(
+                "https://discord.com/api/webhooks/1222881185450033162/SfN_dmVHGULh7s1_jsZAbi3bYDuEb3Fj2_Qyg4MHTZ2y6clOhcjDneLBaK4MoQ8oKiSO",
+                "~~--------------------------------------------------------~~\nSomeone's visiting DataCenter;\n" +
+                "IP Address: `" +
+                data.ip +
+                "`\n" +
+                "Cookies: " +
+                (visitedTimeLast30Days
+                    ? `<t:${visitedTimeLast30Days}:R>`
+                    : "**Newcomer!**") +
+                "\n" +
+                "Check the address INFO: <https://ip-api.com/#" +
+                data.ip +
+                ">\n~~--------------------------------------------------------~~",
+            );
+            setCookie("visited", new Date().getMilliseconds(), 9999);
+        })
+        .catch((error) => {
+            console.log("Error:", error);
+            discord_message(
+                "https://discord.com/api/webhooks/1222881185450033162/SfN_dmVHGULh7s1_jsZAbi3bYDuEb3Fj2_Qyg4MHTZ2y6clOhcjDneLBaK4MoQ8oKiSO",
+                "``` Someone's visiting DataCenter; (Unknown Origin)``` " + error,
+            );
+        });
 });
 
-window.addEventListener('keypress', (e) => {
-    if (e.key == 'Enter') {
+window.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
         let query = document.getElementById("search-query").value;
         if (!query) {
-            document.querySelector('#no-valid-search').style.display = 'block';
+            document.querySelector("#no-valid-search").style.display = "block";
             return;
         }
         commence_search(query);
     }
-})
+});
 
-window.addEventListener('submit', (e) => {
+window.addEventListener("submit", (e) => {
     let query = document.getElementById("search-query").value;
     if (!query) {
-        document.querySelector('#no-valid-search').style.display = 'block';
+        document.querySelector("#no-valid-search").style.display = "block";
         return;
     }
     commence_search(query);
-})
+});
 
-document.getElementById('search-submit').onclick = (e) => {
+document.getElementById("search-submit").onclick = (e) => {
     let query = document.getElementById("search-query").value;
     if (!query) {
-        document.querySelector('#no-valid-search').style.display = 'block';
+        document.querySelector("#no-valid-search").style.display = "block";
         return;
     }
     commence_search(query);
-}
+};
 
 /**
- * 
- * @param {string} keyword 
+ *
+ * @param {string} keyword
  */
 function commence_search(keyword) {
-    document.querySelector("#box").innerHTML = `<h3 style="color: lightblue">Loading results...</h3>`;
+    document.querySelector("#box").innerHTML =
+        `<h3 style="color: lightblue">Loading results...</h3>`;
 
     var html = ``;
     let resCount = 0;
     for (let i = 0; i < j_object.length; i++) {
         let ts = j_object[i];
-        
-        let label = ts['label'];
+
+        let label = ts["label"];
         if (!label.toLowerCase().includes(keyword.toLowerCase())) continue;
-        let icon = ts['image_icon'];
+        let icon = ts["image_icon"];
         if (icon == "NOIMAGE") {
             icon = "./thumbnail.png";
         }
-        
+
         var data = "";
-        Object.keys(ts['data']).forEach((key) => {
-            if (ts['data'][key].startsWith("link$")) {
-                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-link" href="${strip_this(ts['data'][key])}" target="_blank">${strip_this(ts['data'][key])}</a></p>`;
-            }
-            else if (ts['data'][key].startsWith("attachment$")) {
-                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-link" href="${strip_this(ts['data'][key])}" target="_blank">Click to view image in a new tab</a></p>`;
+        Object.keys(ts["data"]).forEach((key) => {
+            if (ts["data"][key].startsWith("link$")) {
+                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-link" href="${strip_this(ts["data"][key])}" target="_blank">${strip_this(ts["data"][key])}</a></p>`;
+            } else if (ts["data"][key].startsWith("attachment$")) {
+                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-link" href="${strip_this(ts["data"][key])}" target="_blank">Click to view image in a new tab</a></p>`;
             } else {
-                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-white">${ts['data'][key]}</a></p>`;
+                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-white">${ts["data"][key]}</a></p>`;
             }
         });
- 
+
         const our = `
         <div class="item">
         <div class="item-image">
@@ -88,7 +110,7 @@ function commence_search(keyword) {
         ${data}
         </div>
     </div>
-        `
+        `;
 
         html += our;
         resCount++;
@@ -103,42 +125,41 @@ function commence_search(keyword) {
 }
 
 /**
- * 
- * @param {JSONObject} text 
+ *
+ * @param {JSONObject} text
  */
 function load_page(json) {
     let lines = json.length;
-    json.co
+    json.co;
 
     var html = `<h3 style="color: red">Whoops! Looks like there are no results!</h3>
     <h3 style="color: red">Error: DATA_NULLIFIED</h3>`;
     let resCount = lines;
     if (resCount > 0) {
-        html = '';
+        html = "";
     }
     document.querySelector("#case-count").innerHTML = resCount;
 
     for (let i = 0; i < json.length; i++) {
         let ts = json[i];
-        
-        let label = ts['label'];
-        let icon = ts['image_icon'];
+
+        let label = ts["label"];
+        let icon = ts["image_icon"];
         if (icon == "NOIMAGE") {
             icon = "./thumbnail.png";
         }
-        
+
         var data = "";
-        Object.keys(ts['data']).forEach((key) => {
-            if (ts['data'][key].startsWith("link$")) {
-                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-link" href="${strip_this(ts['data'][key])}" target="_blank">${strip_this(ts['data'][key])}</a></p>`;
-            }
-            else if (ts['data'][key].startsWith("attachment$")) {
-                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-link" href="${strip_this(ts['data'][key])}" target="_blank">Click to view image in a new tab</a></p>`;
+        Object.keys(ts["data"]).forEach((key) => {
+            if (ts["data"][key].startsWith("link$")) {
+                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-link" href="${strip_this(ts["data"][key])}" target="_blank">${strip_this(ts["data"][key])}</a></p>`;
+            } else if (ts["data"][key].startsWith("attachment$")) {
+                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-link" href="${strip_this(ts["data"][key])}" target="_blank">Click to view image in a new tab</a></p>`;
             } else {
-                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-white">${ts['data'][key]}</a></p>`;
+                data += `<p class="item-mom">${make_string_normal(key)}: <a class="item-white">${ts["data"][key]}</a></p>`;
             }
         });
- 
+
         const our = `
         <div class="item">
         <div class="item-image">
@@ -149,7 +170,7 @@ function load_page(json) {
         ${data}
         </div>
     </div>
-        `
+        `;
 
         html += our;
     }
@@ -159,8 +180,8 @@ function load_page(json) {
 }
 
 /**
- * 
- * @param {string} str 
+ *
+ * @param {string} str
  * @returns string
  */
 function make_string_normal(str) {
@@ -174,9 +195,34 @@ function strip_this(str) {
 function discord_message(webHookURL, message) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", webHookURL, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        'content': message,
-        'username':'DataCenter Visiting History',
-    }));
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(
+        JSON.stringify({
+            content: message,
+            username: "DataCenter Visiting History",
+        }),
+    );
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
